f0adb142bfd7017dc894fb88e48521f2d709b6fc
RomanSoltesz-Revolgy <101571238+RomanSoltesz-Revolgy@users.noreply.github.com>
2024-10-17 10:49:47 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,8 +3,9 @@ resource "aws_instance" "web" {
   instance_type          = "t3.micro"
   key_name               = "react-ec2-key"
   vpc_security_group_ids = [aws_security_group.security_group_react.id]
+  subnet_id              = "subnet-0852ebafdcca26e65"
 
   tags = {
-    Name = "react-app-ec2" #give your ec2 name here
+    Name = "react-app-ec2"
   }
 }
