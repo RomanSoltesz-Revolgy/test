58fb1812e1c88e0cf6879654759ee45eb98db0ed
RomanSoltesz-Revolgy <101571238+RomanSoltesz-Revolgy@users.noreply.github.com>
2024-10-26 19:36:50 +0200

    Update tf_ssp_validation.sh

--- scripts/tf_ssp_validation.sh
+++ scripts/tf_ssp_validation.sh
@@ -1,3 +1,4 @@
+# Based on licence below
 #This solution, non-production-ready template describes AWS Codepipeline based CICD Pipeline for terraform code deployment.
 #© 2023 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
 #This AWS Content is provided subject to the terms of the AWS Customer Agreement available at
@@ -10,8 +11,9 @@
 SKIPVALIDATIONFAILURE=$1
 tfValidate=$2
 tfFormat=$3
-tfCheckov=$4
-tfTfsec=$5
+tfLint=$4
+tfCheckov=$5
+tfTfsec=$6
 # -----------------------------
 
 echo "### VALIDATION Overview ###"
@@ -19,10 +21,14 @@ echo "-------------------------"
 echo "Skip Validation Errors on Failure : ${SKIPVALIDATIONFAILURE}"
 echo "Terraform Validate : ${tfValidate}"
 echo "Terraform Format   : ${tfFormat}"
+echo "Terraform Lint     : ${tfLint}"
 echo "Terraform checkov  : ${tfCheckov}"
 echo "Terraform tfsec    : ${tfTfsec}"
 echo "------------------------"
+
 terraform init
+
+# terraform validate
 if (( ${tfValidate} == "Y"))
 then
     echo "## VALIDATION : Validating Terraform code ..."
@@ -30,6 +36,7 @@ then
 fi
 tfValidateOutput=$?
 
+# terraform fmt -recursive
 if (( ${tfFormat} == "Y"))
 then
     echo "## VALIDATION : Formatting Terraform code ..."
@@ -37,19 +44,28 @@ then
 fi
 tfFormatOutput=$?
 
+# tflint
+if (( ${tfLint} == "Y"))
+then
+    echo "## VALIDATION : Running tflint ..."
+    tflint --init
+    tflint
+fi    
+    tflintOutput=$?
+
+# checkov
 if (( ${tfCheckov} == "Y"))
 then
     echo "## VALIDATION : Running checkov ..."
-    #checkov -s -d .
-    checkov -o junitxml --framework terraform -d ./ >checkov.xml
+    checkov --framework terraform -d ./
 fi
 tfCheckovOutput=$?
 
+# tfsec
 if (( ${tfTfsec} == "Y"))
 then
     echo "## VALIDATION : Running tfsec ..."
-    #tfsec .
-    tfsec ./ --format junit --out tfsec-junit.xml
+    tfsec ./
 fi
 tfTfsecOutput=$?
 
@@ -57,6 +73,7 @@ echo "## VALIDATION Summary ##"
 echo "------------------------"
 echo "Terraform Validate : ${tfValidateOutput}"
 echo "Terraform Format   : ${tfFormatOutput}"
+echo "Terraform Lint     : ${tflintOutput}"
 echo "Terraform checkov  : ${tfCheckovOutput}"
 echo "Terraform tfsec    : ${tfTfsecOutput}"
 echo "------------------------"
@@ -65,11 +82,11 @@ if (( ${SKIPVALIDATIONFAILURE} == "Y" ))
 then
   #if SKIPVALIDATIONFAILURE is set as Y, then validation failures are skipped during execution
   echo "## VALIDATION : Skipping validation failure checks..."
-elif (( $tfValidateOutput == 0 && $tfFormatOutput == 0 && $tfCheckovOutput == 0  && $tfTfsecOutput == 0 ))
+elif (( $tfValidateOutput == 0 && $tfFormatOutput == 0 && $tflintOutput == 0 && $tfCheckovOutput == 0  && $tfTfsecOutput == 0 ))
 then
   echo "## VALIDATION : Checks Passed!!!"
 else
-  # When validation checks fails, build process is halted.
+  # When validation checks fails, build process stops.
   echo "## ERROR : Validation Failed"
   exit 1;
 fi
