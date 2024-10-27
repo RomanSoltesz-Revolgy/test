4443f6539e0f685c483325bab6677157676e74fb
RomanSoltesz-Revolgy <101571238+RomanSoltesz-Revolgy@users.noreply.github.com>
2024-10-27 22:53:23 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami                    = "ami-08ec94f928cf25a9d"
-  instance_type          = "t3.micro"
+  instance_type          = "t3.nano"
   #key_name               = "react-ec2-key"
   vpc_security_group_ids = [aws_security_group.security_group_react.id]
   subnet_id              = "subnet-0852ebafdcca26e65"
